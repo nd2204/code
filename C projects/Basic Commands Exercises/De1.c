6de9783bbdbea7dcf1e0e0fe63612838731bed40
nd2204 <79558656+nd2204@users.noreply.github.com>
2023-01-08 22:13:31 +0700

    Update De1.c

--- C projects/Basic Commands Exercises/De1.c
+++ C projects/Basic Commands Exercises/De1.c
@@ -3,7 +3,8 @@
 
 int main() {
 	int n,m; //n :thoi gian may gia cong 1 man hinh | m: so thiet bi can gia cong
-    
+    	int chiphi;
+	
     while (1) { //yeu cau nhap dung
         printf("nhap thoi gian may gia cong 1 man (trong khoang 1<=n<=60): "); scanf("%d",&n);
         if (n >= 1 && n <= 60) break; else printf ("ban nhap sai\n");
@@ -14,6 +15,8 @@ int main() {
     printf("Tong thoi gian may gia cong cho n thiet bi la: %d", m*n);
     
 	//Dieu kien tinh chi phi gia cong
-    if (n <= 30) printf("\ntong chi phi cho m thiet bi la: %d", 800*m);
-    else printf ("\ntong chi phi cho m thiet bi la: %d", 900*m);
+    if (n <= 30) chiphi=800*m;
+    else chiphi=900*m;
+
+    printf("\nTong chi phi cho m thiet bi la: %d", chiphi);
 }
