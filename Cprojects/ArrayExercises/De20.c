ac89c1fdc17dda5ee6c72c9e87fce35704aaedd2
nd2204 <dn200422@gmail.com>
2023-02-18 23:01:14 +0700

    update

--- Cprojects/ArrayExercises/De20.c
+++ Cprojects/ArrayExercises/De20.c
@@ -20,20 +20,19 @@ int main() {
 		if (i==n-1) printf("}");
 		sum+=array[i];	
 	}
-	printf("\nTong cac phan tu trong mang la: %d",sum);
+	printf("\nTong cac phan tu trong mang la: %.2f",sum);
 	
-	printf("\nNhap X: "); scanf("%f",&x);
-	for (int i=0; i<n; i++) {
-		if (array[i]==x) {
-			count++;
+	printf("\nNhap X: "); scanf("%f",&x); //nhap x
+
+	for (int i=0; i<n; i++){ //cho chay tu a[0]->a[n-1]
+		if (array[i]==x){ //so sanh x voi gia tri thu i trong mang
+				count++;
 			array[i]=0;
-		}
+		}  
 	}
+	
 	printf("Co %d phan tu trong mang bang X\nMang moi co dang: array[%d]={",count,n);
 		for (int i=0; i<n; i++) {
-		printf("%.2f",array[i]);
-		if (i<n-1) printf(",");
-		if (i==n-1) printf("}");
-		sum+=array[i];	
+		printf("%8.2f",array[i]);
 	}
 }
