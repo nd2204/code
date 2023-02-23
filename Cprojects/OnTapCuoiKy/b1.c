ff7ffebab16ca37b8fbe372727eb5d3239fc285c
nd2204 <dn200422@gmail.com>
2023-02-23 14:11:59 +0700

    update

---
+++ Cprojects/OnTapCuoiKy/b1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  float a,b;
+  printf("Nhap a: "); scanf("%f",&a);
+  printf("Nhap b: "); scanf("%f",&b);
+  printf("Cac so da nhap la:\na=%5f\nb=%=12f",a,b);
+
+  printf("\na+b=%f",a+b);
+  printf("\na-b=%f",a-b);
+  printf("\na*b=%f",a*b);
+  printf("\na/b=%f",a/b);
+}
