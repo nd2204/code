80decbf24a59c37edbc68d31e80efdc6ed87f3ea
nd2204 <dn200422@gmail.com>
2023-02-28 19:18:22 +0700

    update

--- Cprojects/OnTapCuoiKy/b4.c
+++ Cprojects/OnTapCuoiKy/b4.c
@@ -5,5 +5,5 @@ int main() {
   float a;
   printf("Nhap a: "); scanf("%f",&a);
   printf("So vua nhap la: %8.2f",a);
-  printf("Gia tri bieu thuc B = %f",log10(a*a)-tanf(3*a));
+  printf("Gia tri bieu thuc B = %f",log(a*a)-tanf(3*a));
 }
